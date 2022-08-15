a6b7b5736ec4733228c05762328b5ce6fd4e4e65
rajaspar <parthiban.rajasekaran@diconium.com>
2022-08-15 18:42:15 +0200

    modify listener implementations

---
+++ cypress.config.js
@@ -0,0 +1,40 @@
+require("dotenv").config({ path: `.env${process.env.ENV_CONFIG}` });
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  modifyObstructiveCode: true,
+  chromeWebSecurity: false,
+
+  defaultCommandTimeout: 8000,
+  pageLoadTimeout: 30000,
+  execTimeout: 60000,
+  requestTimeout: 15000,
+  responseTimeout: 15000,
+
+  reporter: "mochawesome",
+
+  viewportWidth: 1000,
+  viewportHeight: 600,
+  waitForAnimation: true,
+
+  retries: {
+    runMode: 1,
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // config.baseUrl = process.env.URL;
+      config.env.url = process.env.URL;
+      config.env.environment = process.env.ENVIRONMENT;
+      config.env.username = process.env.USERNAME;
+      config.env.password = process.env.PASSWORD;
+      config.env.emailId = process.env.EMAIL_ID;
+
+      console.log(`Tests are running on ${config.env.environment} environment`);
+
+      return config;
+    },
+    specPattern: "cypress/e2e/herokuapp/*.cy.js",
+    supportFile: "cypress/support/e2e.js",
+  },
+});
