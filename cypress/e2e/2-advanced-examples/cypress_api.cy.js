62772757ad4647f06a9a81141436fda2cd800f09
rajaspar <parthiban.rajasekaran@diconium.com>
2022-10-16 20:39:46 +0200

    fix broken assertion

--- cypress/e2e/2-advanced-examples/cypress_api.cy.js
+++ cypress/e2e/2-advanced-examples/cypress_api.cy.js
@@ -91,15 +91,14 @@ context('Cypress.config()', () => {
 
     expect(myConfig).to.have.property('animationDistanceThreshold', 5)
     expect(myConfig).to.have.property('baseUrl', null)
-    expect(myConfig).to.have.property('defaultCommandTimeout', 4000)
-    expect(myConfig).to.have.property('requestTimeout', 5000)
-    expect(myConfig).to.have.property('responseTimeout', 30000)
-    expect(myConfig).to.have.property('viewportHeight', 660)
+    expect(myConfig).to.have.property('defaultCommandTimeout', 8000)
+    expect(myConfig).to.have.property('requestTimeout', 15000)
+    expect(myConfig).to.have.property('responseTimeout', 15000)
+    expect(myConfig).to.have.property('viewportHeight', 600)
     expect(myConfig).to.have.property('viewportWidth', 1000)
-    expect(myConfig).to.have.property('pageLoadTimeout', 60000)
     expect(myConfig).to.have.property('waitForAnimations', true)
 
-    expect(Cypress.config('pageLoadTimeout')).to.eq(60000)
+    expect(Cypress.config('pageLoadTimeout')).to.eq(30000)
 
     // this will change the config for the rest of your tests!
     Cypress.config('pageLoadTimeout', 20000)
