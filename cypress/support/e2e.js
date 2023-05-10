b8a8dc88f53b3e2cb96d1d96bf33e7ac4796b23f
ParthibanRajasekaran <rajasekaran.parthiban7@gmail.com>
2023-05-10 19:42:43 +0100

    feat: adding support files in cypress

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -24,7 +24,18 @@ registerCypressGrep()
 // require('./commands')
 
 Cypress.on("uncaught:exception", (err, runnable) => {
-  // returning false here prevents Cypress from
-  // failing the test
-  return false;
+    // returning false here prevents Cypress from
+    // failing the test
+    return false;
 });
+
+// Hide fetch/XHR requests
+const app = window.top;
+if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
+    const style = app.document.createElement('style');
+    style.innerHTML =
+        '.command-name-request, .command-name-xhr { display: none }';
+    style.setAttribute('data-hide-command-log-request', '');
+
+    app.document.head.appendChild(style);
+}
