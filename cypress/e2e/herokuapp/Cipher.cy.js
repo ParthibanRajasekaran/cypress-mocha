08278f06ca219297a91b2067afc1221aaaa1611a
ParthibanRajasekaran <rajasekaran.parthiban7@gmail.com>
2023-05-22 10:53:27 +0100

    chore: adding sample cipher test

---
+++ cypress/e2e/herokuapp/Cipher.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe('AES Encryption and Decryption', () => {
+    it('should encrypt and then decrypt', () => {
+       // The text to be encrypted.
+        const message = "Hello, world!";
+
+        // Encrypt the text.
+        cy.encrypt(message).then((cipherText) => {
+            cy.log("Encrypted Text is "+message);
+            // Now, let's decrypt it.
+            cy.decrypt(cipherText).then((originalText) => {
+                // Validate the original message and decrypted message are same
+                expect(originalText).to.equal(message);
+                cy.log("Original Text is "+originalText);
+                cy.log("Decrypted Text is "+message);
+            });
+        });
+    });
+});
+
+
